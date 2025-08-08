e35c57c4e1526a5527e1a1a58013128f6443263d
Honey R Shetty <honeyshetty2003@gmail.com>
2025-08-08 14:40:03 +0530

    changes

--- PrintNumbers.java
+++
@@ -1,9 +0,0 @@
-package BasicPrograms;
-
-import java.util.stream.IntStream;
-
-public class PrintNumbers {
-	public static void main(String[] args) {
-        IntStream.range(1, 101).forEach(System.out::println);
-    }
-}
